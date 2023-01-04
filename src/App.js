61147f83f39dd0dc6266af08d8668a7ceefaf0c5
Anurag Sharma <96693300+CrossStack-Q@users.noreply.github.com>
2023-01-04 22:27:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import "./App.css";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+
+import Features from './components/Features';
+import Products from './components/Products';
+
+
+import Categories from './components/Categories';
+import Reviews from './components/Reviews';
+
+
+
+import Blogs from './components/Blogs';
+import Footer from './components/Footer';
+
+function App() {
+  
+
+  return (
+    <main>
+      <Header/>
+      <Banner/>
+      
+      <Features />
+      <Products />
+
+
+      
+      <Categories />
+      <Reviews />
+
+
+
+      <Blogs />
+      <Footer />
+      Hello
+    </main>
+  );
+}
+
+export default App;
